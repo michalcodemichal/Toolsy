190c1974b6225ddeb76f6885e36c24af407ce93d
michalcodemichal <michal.chwastek@pk.edu.pl>
2025-12-30 13:20:38 +0100

    Dodanie podstawowej konfiguracji Spring Boot

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Toolsy
-#Test
---
+++ backend/src/main/java/com/toolsy/ToolsyApplication.java
@@ -0,0 +1,12 @@
+package com.toolsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ToolsyApplication.class, args);
+    }
+}
+
